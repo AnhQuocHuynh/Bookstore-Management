d9f4e946aa6a7a2b1fe70cb5086f3643115053ee
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-11-18 21:35:18 +0700

    feat(cart): add APIs CRUD for cart module

--- apps/api/src/modules/cart/cart.controller.ts
+++ apps/api/src/modules/cart/cart.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CartsService } from './cart.service';
+import { Roles, UserSession } from '@/common/decorators';
+import { UserRole } from '@/modules/users/enums';
+import { CreateCartDto } from '@/common/dtos/cart';
+import { TUserSession } from '@/common/utils';
 
 @Controller('carts')
-export class CartController {}
+export class CartController {
+  constructor(private readonly cartService: CartsService) {}
+
+  @Post()
+  @Roles(UserRole.CUSTOMER)
+  async createCart(
+    @Body() createCartDto: CreateCartDto,
+    @UserSession() userSession: TUserSession,
+  ) {
+    return this.cartService.createNewCart(createCartDto, userSession);
+  }
+}
--- apps/api/src/modules/cart/cart.service.ts
+++ apps/api/src/modules/cart/cart.service.ts
@@ -1,3 +1,6 @@
+import { CreateCartDto } from '@/common/dtos/cart';
+import { TUserSession } from '@/common/utils';
+import { Cart, CartItem } from '@/database/tenant/entities';
 import { TenantService } from '@/tenants/tenant.service';
 import { Injectable } from '@nestjs/common';
 
@@ -5,5 +8,18 @@ import { Injectable } from '@nestjs/common';
 export class CartsService {
   constructor(private readonly tenantService: TenantService) {}
 
-  async createNewCart() {}
+  async createNewCart(createCartDto: CreateCartDto, userSession: TUserSession) {
+    const { bookStoreId } = userSession;
+
+    const dataSource = await this.tenantService.getTenantConnection({
+      bookStoreId,
+    });
+
+    const cartRepo = dataSource.getRepository(Cart);
+    const cartItemRepo = dataSource.getRepository(CartItem);
+
+    const {} = createCartDto;
+  }
+
+  private async createCartItem() {}
 }
