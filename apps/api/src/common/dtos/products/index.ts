c1b25b8c0fe5fc4a400f984bf6b70b9b11cd6f9e
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-12-03 09:33:14 +0700

    feat(products): add endpoint to get product details by id

--- apps/api/src/common/dtos/products/index.ts
+++ apps/api/src/common/dtos/products/index.ts
@@ -1,2 +1,3 @@
 export * from './create-product.dto';
+export * from './get-product-detail-query.dto';
 export * from './get-products-query.dto';
--- apps/api/src/modules/products/products.controller.ts
+++ apps/api/src/modules/products/products.controller.ts
@@ -1,5 +1,9 @@
-import { Roles, UserSession } from '@/common/decorators';
-import { CreateProductDto, GetProductsQueryDto } from '@/common/dtos/products';
+import { BookStoreId, Roles, UserSession } from '@/common/decorators';
+import {
+  CreateProductDto,
+  GetProductDetailQueryDto,
+  GetProductsQueryDto,
+} from '@/common/dtos/products';
 import { TUserSession } from '@/common/utils';
 import { UserRole } from '@/modules/users/enums';
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
@@ -8,6 +12,18 @@ import { ProductsService } from './products.service';
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Get('detail')
+  @Roles(UserRole.EMPLOYEE, UserRole.OWNER)
+  async getProductDetail(
+    @Query() getProductDetailQueryDto: GetProductDetailQueryDto,
+    @BookStoreId() bookStoreId: string,
+  ) {
+    return this.productsService.getProductDetail(
+      getProductDetailQueryDto,
+      bookStoreId,
+    );
+  }
 
   @Post()
   @Roles(UserRole.EMPLOYEE)
