49a5c67632a62ad4b48af632b9f30ff821ea1ead
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-12-15 11:08:49 +0700

    feat(display): add swagger docs and fix display inventory logic

--- apps/api/src/common/dtos/display/create-display-product.dto.ts
+++ apps/api/src/common/dtos/display/create-display-product.dto.ts
@@ -1,18 +1,55 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsPositive, IsUUID } from 'class-validator';
 
 export class CreateDisplayProductDto {
-  @IsNumber()
-  @IsPositive()
+  @ApiProperty({
+    type: Number,
+    description: 'Số lượng cần trưng bày',
+    example: 4,
+  })
+  @IsNumber(
+    {},
+    {
+      message: 'Số lượng trưng bày phải là dạng số',
+    },
+  )
+  @IsPositive({
+    message: 'Số lượng trưng bày phải là số nguyên dương',
+  })
   readonly quantity: number;
 
+  @ApiPropertyOptional({
+    description: 'Thứ tự trưng bày (tuỳ chọn)',
+    type: Number,
+    example: 2,
+  })
   @IsOptional()
-  @IsNumber()
-  @IsPositive()
+  @IsNumber(
+    {},
+    {
+      message: 'Thứ tự trưng bày phải là dạng số',
+    },
+  )
+  @IsPositive({
+    message: 'Thứ tự trưng bày phải là số nguyên dương',
+  })
   readonly displayOrder?: number;
 
-  @IsUUID()
+  @ApiProperty({
+    description: 'Mã ID của sản phẩm',
+    example: 'uuid',
+  })
+  @IsUUID('all', {
+    message: 'Mã ID của sản phẩm không hợp lệ.',
+  })
   readonly productId: string;
 
-  @IsUUID()
+  @ApiProperty({
+    description: 'Mã ID của kệ trưng bày',
+    example: 'uuid',
+  })
+  @IsUUID('all', {
+    message: 'Mã ID của kệ trưng bày không hợp lệ.',
+  })
   readonly displayShelfId: string;
 }
--- apps/api/src/common/dtos/display/move-display-product.dto.ts
+++ apps/api/src/common/dtos/display/move-display-product.dto.ts
@@ -1,11 +1,27 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsInt, IsOptional, IsUUID, Min } from 'class-validator';
 
 export class MoveDisplayProductDto {
-  @IsUUID()
+  @ApiProperty({
+    description: 'Mã ID của kệ cần chuyển đến',
+    example: 'id-1',
+  })
+  @IsUUID('4', {
+    message: 'Mã ID của kệ cần chuyển đến không hợp lệ',
+  })
   readonly targetShelfId: string;
 
+  @ApiPropertyOptional({
+    description:
+      'Số lượng cần chuyển (nếu có), nếu không truyền vào thì mặc định là chuyển toàn bộ.',
+    example: 10,
+  })
   @IsOptional()
-  @IsInt()
-  @Min(1)
+  @IsInt({
+    message: 'Số lượng cần chuyển phải là số nguyên',
+  })
+  @Min(1, {
+    message: 'Số lượng cần chuyển phải ít nhất là 1',
+  })
   readonly quantity?: number;
 }
