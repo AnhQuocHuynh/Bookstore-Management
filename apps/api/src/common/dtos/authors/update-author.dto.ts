468b6484c2870e6808b185394b99227dd8d9f02d
NguyenMinhThien <nguyenminhthienph1449@gmail.com>
2026-01-12 16:06:15 +0700

    feat: api for author

---
+++ apps/api/src/common/dtos/authors/update-author.dto.ts
@@ -0,0 +1,16 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateAuthorDto } from './create-author.dto';
+import { IsEnum, IsOptional } from 'class-validator';
+import { AuthorStatus } from '@/common/enums';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class UpdateAuthorDto extends PartialType(CreateAuthorDto) {
+    @ApiPropertyOptional({
+        description: 'Trạng thái tác giả',
+        enum: AuthorStatus,
+        example: AuthorStatus.ACTIVE,
+    })
+    @IsOptional()
+    @IsEnum(AuthorStatus, { message: 'Trạng thái không hợp lệ' })
+    readonly status?: AuthorStatus;
+}
